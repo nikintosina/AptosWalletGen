f46b405d4283c5097a13745ccd7d8ce24a44ee04
Ghost of Harvard <67272501+nikintosina@users.noreply.github.com>
2023-01-22 12:49:49 -0500

    Update aptos_wallet_gen.py

--- aptos_wallet_gen.py
+++ aptos_wallet_gen.py
@@ -2,12 +2,12 @@ from aptos_sdk.account import Account
 from aptos_sdk.client import FaucetClient, RestClient
 f = open("private_keys_storage.txt", "a")
 
-for i in range(4):
+x = int(input("How many private keys do you want to generate? "))
+
+for i in range(x):
         alice = Account.generate()
         prvk = str(alice.private_key)
         f.write(prvk)
         f.write("\n")
 
 f.close()
-
-
