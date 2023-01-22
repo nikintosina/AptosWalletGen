a558e1db6b552be841b9ff74104740112a4f8df3
Ghost of Harvard <67272501+nikintosina@users.noreply.github.com>
2023-01-22 12:46:54 -0500

    Update aptos_wallet_gen.py
    Changed path

--- aptos_wallet_gen.py
+++ aptos_wallet_gen.py
@@ -1,6 +1,6 @@
 from aptos_sdk.account import Account
 from aptos_sdk.client import FaucetClient, RestClient
-f = open("C:/Users/nickman/aptosproj/myownstuff/private_keys_storage.txt", "a")
+f = open("private_keys_storage.txt", "a")
 
 for i in range(4):
         alice = Account.generate()
